473d8408c117956572058eeedbe8e20ee3ed2f09
FrostHeart21 <FrostHeart21@github.com>
2018-11-19 20:14:39 +0000

    Inicio de navegaçao entre paginas; Alteraçoes na title bar.

--- app/src/main/java/com/vigilanteyes/ua/vigilanteyes/LoginScreen.java
+++ app/src/main/java/com/vigilanteyes/ua/vigilanteyes/LoginScreen.java
@@ -1,7 +1,11 @@
 package com.vigilanteyes.ua.vigilanteyes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.vigilanteyes.ua.vigilanteyes.Security.Profile;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+    }
+
+    public void loginBtnPressed(View view) {
+        Intent in = new Intent(this, Profile.class);
+        startActivity(in);
     }
 }
--- app/src/main/java/com/vigilanteyes/ua/vigilanteyes/Security/Profile.java
+++ app/src/main/java/com/vigilanteyes/ua/vigilanteyes/Security/Profile.java
@@ -1,8 +1,13 @@
 package com.vigilanteyes.ua.vigilanteyes.Security;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.vigilanteyes.ua.vigilanteyes.LoginScreen;
 import com.vigilanteyes.ua.vigilanteyes.R;
 
 public class Profile extends AppCompatActivity {
@@ -11,5 +16,26 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            case R.id.logout:
+                startActivity(new Intent(this,LoginScreen.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
