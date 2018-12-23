b5c490617b3dfb6b41c295016aeb7dc8ddc22782
FrostHeart21 <FrostHeart21@github.com>
2018-12-23 19:44:27 +0000

    Criação da homepage dos supervisores

---
+++ app/src/main/java/com/vigilanteyes/ua/vigilanteyes/Supervisor/HomeSupervisor.java
@@ -0,0 +1,31 @@
+package com.vigilanteyes.ua.vigilanteyes.Supervisor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.vigilanteyes.ua.vigilanteyes.R;
+
+public class HomeSupervisor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_supervisor);
+    }
+
+    public void rotaBtnPressed(View view) {
+    }
+
+    public void segurancasBtnPressed(View view) {
+    }
+
+    public void relatoriosBtnPressed(View view) {
+    }
+
+    public void perfilBtnPressed(View view) {
+    }
+
+    public void logoutBtnPressed(View view) {
+    }
+}
